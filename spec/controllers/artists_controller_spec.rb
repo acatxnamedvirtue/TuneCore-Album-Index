79c25e87a38fb8c6ecb47fab45c5823799cc6a76
Tyler Beebe <tsb252@gmail.com>
2017-05-14 15:22:18 -0400

    Replace double quotes with single quotes, update routing specs with search test

--- spec/controllers/artists_controller_spec.rb
+++ spec/controllers/artists_controller_spec.rb
@@ -7,79 +7,79 @@ RSpec.describe ArtistsController, type: :controller do
 
   let(:valid_session) {{}}
 
-  describe "GET #index" do
-    it "returns a success response" do
+  describe 'GET #index' do
+    it 'returns a success response' do
       artist = Artist.create! valid_attributes
       get :index, params: {}, session: valid_session
       expect(response).to be_success
     end
   end
 
-  describe "GET #show" do
-    it "returns a success response" do
+  describe 'GET #show' do
+    it 'returns a success response' do
       artist = Artist.create! valid_attributes
       get :show, params: {id: artist.to_param}, session: valid_session
       expect(response).to be_success
     end
   end
 
-  describe "GET #new" do
-    it "returns a success response" do
+  describe 'GET #new' do
+    it 'returns a success response' do
       get :new, params: {}, session: valid_session
       expect(response).to be_success
     end
   end
 
-  describe "GET #edit" do
-    it "returns a success response" do
+  describe 'GET #edit' do
+    it 'returns a success response' do
       artist = Artist.create! valid_attributes
       get :edit, params: {id: artist.to_param}, session: valid_session
       expect(response).to be_success
     end
   end
 
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Artist" do
+  describe 'POST #create' do
+    context 'with valid params' do
+      it 'creates a new Artist' do
         expect {
           post :create, params: {artist: valid_attributes}, session: valid_session
         }.to change(Artist, :count).by(1)
       end
 
-      it "redirects to the created artist" do
+      it 'redirects to the created artist' do
         post :create, params: {artist: valid_attributes}, session: valid_session
         expect(response).to redirect_to(Artist.last)
       end
     end
 
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'new' template)" do
+    context 'with invalid params' do
+      it 'returns a success response (i.e. to display the \'new\' template)' do
         post :create, params: {artist: invalid_attributes}, session: valid_session
         expect(response).to be_success
       end
     end
   end
 
-  describe "PUT #update" do
-    context "with valid params" do
+  describe 'PUT #update' do
+    context 'with valid params' do
       let(:new_attributes) {{name: 'updated name'}}
 
-      it "updates the requested artist" do
+      it 'updates the requested artist' do
         artist = Artist.create! valid_attributes
         put :update, params: {id: artist.to_param, artist: new_attributes}, session: valid_session
         artist.reload
         expect(artist.name).to eq 'updated name'
       end
 
-      it "redirects to the artist" do
+      it 'redirects to the artist' do
         artist = Artist.create! valid_attributes
         put :update, params: {id: artist.to_param, artist: valid_attributes}, session: valid_session
         expect(response).to redirect_to(artist)
       end
     end
 
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'edit' template)" do
+    context 'with invalid params' do
+      it 'returns a success response (i.e. to display the \'edit\' template)' do
         artist = Artist.create! valid_attributes
         put :update, params: {id: artist.to_param, artist: invalid_attributes}, session: valid_session
         expect(response).to be_success
@@ -87,15 +87,15 @@ RSpec.describe ArtistsController, type: :controller do
     end
   end
 
-  describe "DELETE #destroy" do
-    it "destroys the requested artist" do
+  describe 'DELETE #destroy' do
+    it 'destroys the requested artist' do
       artist = Artist.create! valid_attributes
       expect {
         delete :destroy, params: {id: artist.to_param}, session: valid_session
       }.to change(Artist, :count).by(-1)
     end
 
-    it "redirects to the artists list" do
+    it 'redirects to the artists list' do
       artist = Artist.create! valid_attributes
       delete :destroy, params: {id: artist.to_param}, session: valid_session
       expect(response).to redirect_to(artists_url)
--- spec/controllers/songs_controller_spec.rb
+++ spec/controllers/songs_controller_spec.rb
@@ -19,81 +19,81 @@ RSpec.describe SongsController, type: :controller do
 
   let(:valid_session) {{}}
 
-  describe "GET #index" do
-    it "returns a success response" do
+  describe 'GET #index' do
+    it 'returns a success response' do
       song = Song.create! valid_attributes
       get :index, params: {}, session: valid_session
       expect(response).to be_success
     end
   end
 
-  describe "GET #show" do
-    it "returns a success response" do
+  describe 'GET #show' do
+    it 'returns a success response' do
       song = Song.create! valid_attributes
       get :show, params: {id: song.to_param}, session: valid_session
       expect(response).to be_success
     end
   end
 
-  describe "GET #new" do
-    it "returns a success response" do
+  describe 'GET #new' do
+    it 'returns a success response' do
       get :new, params: {}, session: valid_session
       expect(response).to be_success
     end
   end
 
-  describe "GET #edit" do
-    it "returns a success response" do
+  describe 'GET #edit' do
+    it 'returns a success response' do
       song = Song.create! valid_attributes
       get :edit, params: {id: song.to_param}, session: valid_session
       expect(response).to be_success
     end
   end
 
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Song" do
+  describe 'POST #create' do
+    context 'with valid params' do
+      it 'creates a new Song' do
         expect {
           post :create, params: {song: create_attributes}, session: valid_session
         }.to change(Song, :count).by(1)
       end
 
-      it "redirects to the created song" do
+      it 'redirects to the created song' do
         post :create, params: {song: create_attributes}, session: valid_session
         expect(response).to redirect_to(Song.last)
       end
     end
 
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'new' template)" do
+    context 'with invalid params' do
+      it 'returns a success response (i.e. to display the \'new\' template)' do
         post :create, params: {song: invalid_create_attributes}, session: valid_session
         expect(response).to be_success
       end
     end
   end
 
-  describe "PUT #update" do
-    context "with valid params" do
+  describe 'PUT #update' do
+    context 'with valid params' do
       let(:new_attributes) {
         {title: 'updated title'}
       }
 
-      it "updates the requested song" do
+      it 'updates the requested song' do
         song = Song.create! valid_attributes
         put :update, params: {id: song.to_param, song: new_attributes}, session: valid_session
         song.reload
         expect(song.title).to eq 'updated title'
       end
 
-      it "redirects to the song" do
+      it 'redirects to the song' do
         song = Song.create! valid_attributes
         put :update, params: {id: song.to_param, song: valid_attributes}, session: valid_session
         expect(response).to redirect_to(song)
       end
     end
 
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'edit' template)" do
+    context 'with invalid params' do
+      it 'returns a success response (i.e. to display the \'edit\' template)' do
         song = Song.create! valid_attributes
         put :update, params: {id: song.to_param, song: invalid_attributes}, session: valid_session
         expect(response).to be_success
@@ -101,29 +101,29 @@ RSpec.describe SongsController, type: :controller do
     end
   end
 
-  describe "DELETE #destroy" do
-    it "destroys the requested song" do
+  describe 'DELETE #destroy' do
+    it 'destroys the requested song' do
       song = Song.create! valid_attributes
       expect {
         delete :destroy, params: {id: song.to_param}, session: valid_session
       }.to change(Song, :count).by(-1)
     end
 
-    it "redirects to the songs list" do
+    it 'redirects to the songs list' do
       song = Song.create! valid_attributes
       delete :destroy, params: {id: song.to_param}, session: valid_session
       expect(response).to redirect_to(songs_url)
     end
   end
 
-  describe "SEARCH #search" do
-    it "returns a success response with no search param" do
+  describe 'SEARCH #search' do
+    it 'returns a success response with no search param' do
       song = Song.create! valid_attributes
       get :search, params: {}, session: valid_session
       expect(response).to be_success
     end
 
-    it "returns a success response with a search paramm" do
+    it 'returns a success response with a search paramm' do
       song = Song.create! valid_attributes
       post :search, params: {search: song.title}, session: valid_session
       expect(response).to be_success
