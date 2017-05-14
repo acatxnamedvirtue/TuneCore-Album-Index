fc93e2eff6a690f7cc2ce401f8792f3baf11b28e
Tyler Beebe <tsb252@gmail.com>
2017-05-14 12:47:43 -0400

    Add favicons and realistic seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+genre = ['Rock', 'Pop', 'Punk', 'Indie', 'Alternative']
+
+10.times {
+  artist = Artist.create(name: Faker::RockBand.name)
+  10.times {
+    album = Album.create(title: Faker::Book.title,
+                         artist_id: artist.id,
+                         genre: genre[Random.rand(5)],
+                         year: Faker::Date.between(70.years.ago, Date.today).year)
+    5.times do |x|
+      Song.create(title: Faker::Book.title,
+                  track_number: x+1,
+                  length: Faker::Number.between(60, 600),
+                  album_id: album.id)
+    end
+  }
+}
+
+
+
